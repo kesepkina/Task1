bd6619d26a95f42d9e917d84f0d8707c8d4182ea
Esepkina Kseniya <k.esepkina@gmail.com>
2020-11-12 11:54:20 +0300

    Day 1 done

--- src/com/epam/task1/assignment5/service/NumberService.java
+++ src/com/epam/task1/assignment5/service/NumberService.java
@@ -1,7 +1,7 @@
 package com.epam.task1.assignment5.service;
 
 
-import com.epam.task1.assignment4.exception.NumberServiceException;
+import com.epam.task1.assignment5.exception.NumberServiceException;
 
 public class NumberService {
     public boolean isPerfectNumber(int number) throws NumberServiceException {
