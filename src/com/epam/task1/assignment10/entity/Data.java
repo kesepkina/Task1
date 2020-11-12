9499550036582199fba85c493ddce87dee8bc128
Esepkina Kseniya <k.esepkina@gmail.com>
2020-11-12 11:48:12 +0300

    added .gitignore file

---
+++ src/com/epam/task1/assignment10/entity/Data.java
@@ -0,0 +1,9 @@
+package com.epam.task1.assignment10.entity;
+
+public class Data {
+    double datas[][];
+
+    public Data(double[][] datas) {
+        this.datas = datas;
+    }
+}
---
+++ src/com/epam/task1/assignment5/exception/NumberServiceException.java
@@ -0,0 +1,2 @@
+package com.epam.task1.assignment5.exception;public class NumberServiceException {
+}
