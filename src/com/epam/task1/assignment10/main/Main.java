2cf0382a5e47f1de8ec6712f9cbe1cdd4fc5f3fd
Kseniya <43503787+kesepkina@users.noreply.github.com>
2020-11-13 18:07:24 +0300

    Add files via upload

--- src/com/epam/task1/assignment10/main/Main.java
+++ src/com/epam/task1/assignment10/main/Main.java
@@ -1,13 +1,16 @@
 package com.epam.task1.assignment10.main;
 
+import com.epam.task1.assignment10.entity.Data;
 import com.epam.task1.assignment10.report.MathReport;
 import com.epam.task1.assignment10.service.MathService;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MathService service = new MathService();
-        double[][] data = service.calcFunction(0.785, 9.42, 0.785 );
         MathReport report = new MathReport();
-        report.showResults(data);
+
+        Data arguments = service.calcInitialData(Math.PI / 4, 4 * Math.PI, Math.PI / 2);
+        Data results = service.calcFunction(arguments);
+        report.showResults(arguments, results);
     }
 }
--- src/com/epam/task1/assignment10/service/MathService.java
+++ src/com/epam/task1/assignment10/service/MathService.java
@@ -1,16 +1,27 @@
 package com.epam.task1.assignment10.service;
 
+import com.epam.task1.assignment10.entity.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MathService {
-    public List<double[]> calcFunction(double a, double b, double h) {
-        List<double[]> data = new ArrayList<double[]>();
-        int i = 0;
-        for (double j = a; j < b + h; j += h) {
-            data.add(new double[]{j,Math.tan(j)});
-            i++;
+    public Data calcFunction(Data arguments) {
+        List<Double> results = new ArrayList<>();
+        List<Double> initNumbers = arguments.getNumbers();
+        for (double nextData : initNumbers) {
+            results.add(Math.tan(nextData));
         }
-        return data;
+        return new Data(results);
+    }
+
+    public Data calcInitialData(double a, double b, double h) {
+        List<Double> data = new ArrayList<>();
+        double nextData = a;
+        while (nextData <= b) {
+            data.add(nextData);
+            nextData += h;
+        }
+        return new Data(data);
     }
 }
