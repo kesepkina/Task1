06942c20f2b0311b4719b519fdae04970f2351ff
Esepkina Kseniya <k.esepkina@gmail.com>
2020-11-13 18:03:39 +0300

    Fixed bugs

--- src/com/epam/task1/assignment10/entity/Data.java
+++ src/com/epam/task1/assignment10/entity/Data.java
@@ -1,9 +1,47 @@
 package com.epam.task1.assignment10.entity;
 
-public class Data {
-    double datas[][];
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Objects;
 
-    public Data(double[][] datas) {
-        this.datas = datas;
+public class Data {
+    private List<Double> numbers;
+
+    public Data(List<Double> numbers) {
+        this.numbers = numbers;
     }
+
+    public List<Double> getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(List<Double> numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Data data = (Data) o;
+
+        if (numbers.size() != data.numbers.size()) {
+            return false;
+        }
+
+        DecimalFormat df = new DecimalFormat("#.####");
+        for (int i = 0; i < numbers.size(); i++) {
+            if (!df.format(numbers.get(i)).equals(df.format(data.numbers.get(i)))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
+    }
+
 }
--- src/com/epam/task1/assignment10/report/MathReport.java
+++ src/com/epam/task1/assignment10/report/MathReport.java
@@ -1,11 +1,17 @@
 package com.epam.task1.assignment10.report;
 
+import com.epam.task1.assignment10.entity.Data;
+
 import java.util.List;
 
 public class MathReport {
-   /* public void showResults(List<double[]> data) {
-        for (int i = 0; i < data.length; i++) {
-            System.out.println("\t" + data[0][i] + "\t" + data[1][i] + "\n");
+    public void showResults(Data initialData, Data resultData) {
+        List<Double> arguments = initialData.getNumbers();
+        List<Double> results = resultData.getNumbers();
+
+        System.out.println("\tArgument\tResult");
+        for (int i = 0; i < arguments.size(); i++) {
+            System.out.printf("\t%.3f\t\t%.3f%n", arguments.get(i), results.get(i));
         }
-    }*/
+    }
 }
